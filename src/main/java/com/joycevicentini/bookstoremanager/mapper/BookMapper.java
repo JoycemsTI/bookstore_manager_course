8acd3f1f114b72325cb6b628e1f425eb8709d7f7
Joyce Vicentini <joy.vicentini@gmail.com>
2021-07-30 11:55:23 -0300

    Imcremento de Swagger na aplicação!

--- src/main/java/com/joycevicentini/bookstoremanager/mapper/BookMapper.java
+++ src/main/java/com/joycevicentini/bookstoremanager/mapper/BookMapper.java
@@ -3,12 +3,12 @@ package com.joycevicentini.bookstoremanager.mapper;
 import com.joycevicentini.bookstoremanager.dto.BookDTO;
 import com.joycevicentini.bookstoremanager.entity.Book;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+// import org.mapstruct.factory.Mappers;
 
-@Mapper
+
 public interface BookMapper {
 
-    BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
+   // BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
 
     Book toModel(BookDTO bookDTO);
 
